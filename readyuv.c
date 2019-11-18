2ca10159cf3efd51a1f624467d2fce98bbc63855
shimoniida <csru15054@g.nihon-u.ac.jp>
2019-11-18 13:03:49 +0900

    core dump

--- readyuv.c
+++ readyuv.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
-#include "tiffio.h"
-#include"tiff.h"
-
-
 
 int main(int argc, char *argv[]){
   FILE *origin;
+  //FILE *compare;
   int width = 0;
   int height = 0;
   int read_size = 0;
@@ -15,34 +12,48 @@ int main(int argc, char *argv[]){
   int size = 0;
   int data = 0;
   unsigned char *filesize = NULL;
+  //unsigned char *ofsize = NULL;
 
   width = atoi(argv[2]);
   height = atoi(argv[3]);
   nbframes = atoi(argv[4]);
-  printf("%d",width);
+  //printf("%d",width);
 
   if(NULL ==(origin=fopen(argv[1],"rb"))){
     fprintf(stderr,"Can not open input file\n");
     exit(1);
   }
+  /*if(NULL ==(compare=fopen(argv[2],"wb"))){
+    fprintf(stderr,"Can not open output file\n");
+    exit(1);
+  }*/
 
   filesize = (unsigned char*)malloc((sizeof(unsigned char)*width*height*nbframes));
-  if(filesize==NULL){
+  
+  if((filesize==NULL)){//||(ofsize==NULL)){
     printf("malloc error\n");
+    fflush(stdout);
     free(filesize);
     exit(1);
   }
   
   memset(filesize,0,sizeof(unsigned char)*width*height*nbframes);
+  //memset(ofsize,0,sizeof(unsigned char)*width*height*nbframes);
 
   read_size = width;
-
-  for(int i=0; i<nbframes;i++){
-    for(int j=0;j<height;j++){
-      fread(filesize,1,read_size,origin);
+ int i,j;
+ //int ret;
+  for(i=0; i<nbframes;i++){
+    for(j=0;j<height;j++){
+      int fs;
+      fs = fread(filesize,1,read_size,origin);
       printf("%d\n",*filesize);
+      //fs++;
     }
     origin += width*height/2;
   }
+  
+  //fwrite(filesize,read_size,1,compare);
+  
   return 0;
 }
